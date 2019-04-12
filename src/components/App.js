a7804c806d2e09d2d64e41f855b3cff0592cbae9
VitalGol <vital_gol@ukr.net>
2019-04-12 10:40:55 +0300

    third step

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Dawer from "./Dawer";
 import "./App.css";
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello {this.props.name}</h1>
+        <Dawer />
       </div>
     );
   }
